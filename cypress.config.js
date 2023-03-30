180c44665e2a638316982f42d476b42275208efa
Schneider <gustavo.schneider@me.com.br>
2023-03-30 11:01:19 -0300

    Configure cypress dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,14 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  e2e: {
-    baseUrl: 'https://notes-serverless-app.com',
-    env: {
-      viewportWidthBreakpoint: 768
-    },
-  },
-  defaultCommandTimeout: 10000,
-  chromeWebSecurity: false,
-  experimentalStudio: true
+	projectId: "y94epb",
+	e2e: {
+		baseUrl: 'https://notes-serverless-app.com',
+		env: {
+			viewportWidthBreakpoint: 768
+		},
+	},
+	defaultCommandTimeout: 10000,
+	chromeWebSecurity: false,
+	experimentalStudio: true
 })
